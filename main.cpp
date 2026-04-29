78a6e7997ab5a8ec1e9604686bcdf8d0650fdfc8
Ali Rouhi <thealirouhi@gmail.com>
2026-04-29 21:15:26 +0330

    completing the methods and classes

--- main.cpp
+++ main.cpp
@@ -16,17 +16,9 @@ public:
     Task(int taskId, const string& taskTitle)
         : id(taskId), title(taskTitle), done(false) {}
 
-    int getId() const {
-        return id;
-    }
-
-    string getTitle() const {
-        return title;
-    }
-
-    bool isDone() const {
-        return done;
-    }
+    int getId() const { return id; }
+    string getTitle() const { return title; }
+    bool isDone() const { return done; }
 
     void markDone() {
         done = true;
@@ -59,17 +51,60 @@ public:
     void createList(const string& listName) {
         name = listName;
         tasks.clear();
-        cout << "List '" << name << "' created." << endl;
+        cout << "List '" << name << "' created.\n";
     }
 
     void addTask(int id, const string& title) {
         if (findTask(id) != nullptr) {
-            cout << "Error: task " << id << " already exists." << endl;
+            cout << "Error: task " << id << " already exists.\n";
             return;
         }
 
-        tasks.push_back(Task(id, title));
-        cout << "Task " << id << " added." << endl;
+        tasks.emplace_back(id, title);
+        cout << "Task " << id << " added.\n";
+    }
+
+    void listTasks() {
+        if (tasks.empty()) {
+            cout << "No tasks.\n";
+            return;
+        }
+
+        for (const auto& task : tasks) {
+            cout << task.getId() << " | "
+                 << task.getTitle() << " | "
+                 << (task.isDone() ? "done" : "pending");
+
+            auto deps = task.getDependencies();
+            if (!deps.empty()) {
+                cout << " | deps: ";
+                for (int d : deps) cout << d << " ";
+            }
+
+            cout << "\n";
+        }
+    }
+
+    void markDone(int id) {
+        Task* task = findTask(id);
+        if (!task) {
+            cout << "Task not found.\n";
+            return;
+        }
+
+        task->markDone();
+        cout << "Task " << id << " marked done.\n";
+    }
+
+    void addDependency(int taskId, int depId) {
+        Task* task = findTask(taskId);
+        if (!task) {
+            cout << "Task not found.\n";
+            return;
+        }
+
+        task->addDependency(depId);
+        cout << "Dependency added: " << taskId << " -> " << depId << "\n";
     }
 };
 
@@ -85,16 +120,39 @@ int main() {
         if (command == "EXIT") {
             break;
         }
+
         else if (command == "NEW_LIST") {
             string listName;
             ss >> listName;
             todo.createList(listName);
         }
+
         else if (command == "ADD") {
             int id;
+            ss >> id;
+
             string title;
-            ss >> id >> title;
+            getline(ss, title);
+            if (!title.empty() && title[0] == ' ')
+                title = title.substr(1);
+
             todo.addTask(id, title);
+        }
+
+        else if (command == "LIST") {
+            todo.listTasks();
+        }
+
+        else if (command == "DONE") {
+            int id;
+            ss >> id;
+            todo.markDone(id);
+        }
+
+        else if (command == "DEP") {
+            int taskId, depId;
+            ss >> taskId >> depId;
+            todo.addDependency(taskId, depId);
         }
     }
 
